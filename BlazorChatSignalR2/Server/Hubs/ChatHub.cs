7e94210b9206fbe0cd0b7392242320358a859c43
Ezra.Ombaso <Ezra.Ombaso@LTHSEIT052289>
2023-10-03 13:05:58 +0300

    Add project files.

---
+++ BlazorChatSignalR2/Server/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR2.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public override async Task OnConnectedAsync() 
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} joined the party!");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username =Users.FirstOrDefault(u=> u.Key== Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left!");
+        }
+
+        public async Task AddMessageToChat(String User, String Message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageBro",User, Message);
+        }
+    }
+}
